df691b508712d17bf3e9662f5bd98aabf83dc238
Jonghyeon Ko <61593290+flowithus@users.noreply.github.com>
2021-04-24 03:14:27 +0900

    Chakra UI installed (Something problem on Chakra, version of Storybook)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { addDecorator } from "@storybook/react";
 import Center from "../src/components/Center/Center";
+import { ThemeProvider, theme, CSSReset, Box } from "@chakra-ui/core";
 
 // Globally Decorate Story
 addDecorator((story) => <Center>{story()} </Center>);
@@ -20,3 +21,14 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Box m="4">
+        <Story />
+      </Box>
+    </ThemeProvider>
+  ),
+];
