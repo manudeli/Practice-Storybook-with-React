050824b15bb30ede482cf8067da2abc15e9e516c
Jonghyeon Ko <61593290+flowithus@users.noreply.github.com>
2021-04-24 03:33:19 +0900

    Addon controls actions viewport etc.

--- src/components/ChakraButton/ChakraButton.stories.js
+++ src/components/ChakraButton/ChakraButton.stories.js
@@ -4,7 +4,21 @@ import { Button } from "@chakra-ui/core";
 export default {
   title: "Chakra/Button",
   component: Button,
+  argTypes: {
+    variantColor: { control: "text" },
+    children: { control: "text" },
+    onClick: { action: "clicked" },
+  },
 };
 
-export const Success = () => <Button variantColor="green">Success</Button>;
-export const Danger = () => <Button variantColor="red">Danger</Button>;
+const Template = (args) => <Button {...args} />;
+export const Success = Template.bind({});
+Success.args = {
+  variantColor: "green",
+  children: "Success",
+};
+export const Danger = Template.bind({});
+Danger.args = {
+  variantColor: "red",
+  children: "Danger",
+};
