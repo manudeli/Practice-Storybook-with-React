c44aacb297c341f5212eaad9606827849ed30783
Jonghyeon Ko <61593290+flowithus@users.noreply.github.com>
2021-04-24 02:31:37 +0900

    Args for Storybook

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,11 +2,31 @@ import React from "react";
 import Button from "./Button";
 
 export default {
-  title: "Button",
+  title: "form/control/Button",
   component: Button,
+  args: {
+    children: "Button",
+  },
 };
 
 export const Primary = () => <Button variant="primary">Primary</Button>;
 export const Secondary = () => <Button variant="secondary">Secondary</Button>;
 export const Success = () => <Button variant="success">Success</Button>;
 export const Danger = () => <Button variant="danger">Danger</Button>;
+
+const Template = (args) => <Button {...args} />;
+
+export const PrimaryA = Template.bind({});
+PrimaryA.args = {
+  variant: "primary",
+};
+
+export const LongPrimaryA = Template.bind({});
+LongPrimaryA.args = {
+  ...PrimaryA.args,
+};
+
+export const SecondaryA = Template.bind({});
+SecondaryA.args = {
+  variant: "secondary",
+};
--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -2,10 +2,14 @@ import React from "react";
 import Input from "./Input";
 
 export default {
-  title: "Input",
+  title: "form/control/Input",
   component: Input,
 };
 
 export const Small = () => <Input size="small" placeholder="Small size" />;
 export const Medium = () => <Input size="medium" placeholder="Medium size" />;
 export const Large = () => <Input size="large" placeholder="Large size" />;
+
+Small.storyName = "Small Input";
+Medium.storyName = "Medium Input";
+Large.storyName = "Large Input";
